19273c98b24cf4e3de8c8dffe4c2d91b843cbf4c
Chomba Ng'ang'a <choeringa@gmail.com>
2014-01-22 22:16:33 +0300

    ScrapydCommunicator working :-), able to start crawls via web interface

--- johnnywalker/views.py
+++ johnnywalker/views.py
@@ -1,9 +1,8 @@
 # Create your views here.
 from json import loads
-
 from urllib2 import urlopen
+from urlparse import urljoin
 
-from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.http import urlencode
@@ -14,6 +13,8 @@ from johnnywalker.forms import StartCrawlForm
 class ScrapydCommunicator(object):
     host = '127.0.0.1'
     port = 6880
+    spider = 'walker'
+    project = 'johnnywalker'
 
     def __init__(self):
         pass
@@ -24,8 +25,8 @@ class ScrapydCommunicator(object):
         if method in ['POST', 'post']:
             return loads(urlopen(url, data_encoded).read())
         else:
-            data = url + ("?" + data_encoded) if len(data_encoded) > 0 else ""
-            return loads(urlopen(url).read())
+            data = ("?" + data_encoded) if len(data_encoded) > 0 else ""
+            return loads(urlopen(urljoin(url, data)).read())
 
     def listprojects(self):
         """
@@ -45,7 +46,7 @@ class ScrapydCommunicator(object):
         """
         pass
 
-    def schedule(self):
+    def schedule(self, startpage, domain):
         """
         POST
         - project (string, required) - the project name
@@ -55,15 +56,19 @@ class ScrapydCommunicator(object):
 
         sample response: {"status": "ok", "jobid": "6487ec79947edab326d6db28a2d86511e8247444"}
         """
-        pass
+        return self._get_jsondata('schedule', 'post',
+                                  {'project': self.project, "spider": self.spider,
+                                   "start": startpage, "domain": domain
+                                  })
 
-    def cancel(self):
+    def cancel(self, jobid):
         """
         POST
         - project (string, required) - the project name
         - job (string, required) - the job id
         sample response: {"status": "ok", "prevstate": "running"}
         """
+        return self._get_jsondata('cancel', 'post', {'project': self.project, 'job': jobid})
 
     def listversions(self):
         """
@@ -71,7 +76,7 @@ class ScrapydCommunicator(object):
         - project (string, required) - the project name
         sample response: {"status": "ok", "versions": ["r99", "r156"]}
         """
-        pass
+        return self._get_jsondata('listversions', 'get', {'project': self.project})
 
     def listspiders(self):
         """
@@ -79,7 +84,7 @@ class ScrapydCommunicator(object):
         - project (string, required) - the project name
         sample response: {"status": "ok", "spiders": ["spider1", "spider2", "spider3"]}
         """
-        pass
+        return self._get_jsondata('listspiders', 'get', {'project': self.project})
 
     def listjobs(self):
         """
@@ -90,16 +95,16 @@ class ScrapydCommunicator(object):
         "running": [{"id": "422e608f9f28cef127b3d5ef93fe9399", "spider": "spider2", "start_time": "2012-09-1
         "finished": [{"id": "2f16646cfcaf11e1b0090800272a6d06", "spider": "spider3", "start_time": "2012-09-
         """
-        pass
+        return self._get_jsondata('listjobs', 'get', {'project': self.project})
 
-    def delversion(self):
+    def delversion(self, version):
         """
         POST
         - project (string, required) - the project name
         - version (string, required) - the project version
         sample response: {"status": "ok"}
         """
-        pass
+        return self._get_jsondata('delversion', 'get', {'project': self.project, 'version': version})
 
     def delproject(self):
         """
@@ -108,26 +113,21 @@ class ScrapydCommunicator(object):
 
         sample response: {"status": "ok"}
         """
-        pass
+        return self._get_jsondata('delproject', 'get', {'project': self.project})
 
 
 def home(request):
     frm = StartCrawlForm(request.POST or None)
     comms = ScrapydCommunicator()
+    import pdb;pdb.set_trace()
     projects = comms.listprojects()
+    jobs = comms.listjobs()
+    status = {}
     if frm.is_valid():
         startpage, domain = frm.cleaned_data['startpage'], frm.cleaned_data['domain']
-        return render_to_response('johnnywalker/home.html', {'active_spiders': projects})
+        status = comms.schedule(startpage, domain)
 
-    return render_to_response('johnnywalker/home.html', {'startcrawlform': frm, 'active_spiders': projects},
+    return render_to_response('johnnywalker/home.html',
+                              {'startcrawlform': frm, 'projects': projects, 'jobs': jobs, 'status': status},
                               context_instance=RequestContext(request))
-
-
-def start(request):
-    return HttpResponse('spider started')
-
-
-def pause(request):
-    pass
-
 
