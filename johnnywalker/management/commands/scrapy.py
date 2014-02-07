98a6f8e4de18d6581586f4117f50bfed6ae125fe
Chomba Ng'ang'a <choeringa@gmail.com>
2014-02-07 08:25:24 +0300

    Dummy proofing scrapy command

--- johnnywalker/management/commands/scrapy.py
+++ johnnywalker/management/commands/scrapy.py
@@ -6,8 +6,9 @@ from scrapy.cmdline import execute
 
 from . import get_scrapyroot
 
+
 class Command(BaseCommand):
-    help = 'Run scrapy within django'
+    help = 'Run scrapy within django\nUsage : python manage.py scrapy start=<start page> domain=<domain url>'
 
     def run_from_argv(self, argv):
         self._argv = argv
@@ -16,9 +17,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         scrapydir = get_scrapyroot()
         chdir(scrapydir)
-        default_args = ['scrapy','crawl', 'walker']
-        default_args.extend(['-a',self._argv[2]])
-        default_args.extend(['-a',self._argv[3]])
+        if len(self._argv) != 4:
+            self.stdout.write(self.help)
+            return
+        default_args = ['scrapy', 'crawl', 'walker']
+        default_args.extend(['-a', self._argv[2]])
+        default_args.extend(['-a', self._argv[3]])
         execute(default_args)
         self.stdout.write(str(args))
         self.stdout.write(str(options))
