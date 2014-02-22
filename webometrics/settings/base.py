d066729777879a9eb7f00a9b1c3a182ff9daca3c
Chomba Ng'ang'a <choeringa@gmail.com>
2014-02-22 06:22:47 +0300

    Removing tastypie settings in settings

--- webometrics/settings/base.py
+++ webometrics/settings/base.py
@@ -164,11 +164,3 @@ LOGGING = {
 
 # CSRF: this is the cookie name expected by angular.js
 CSRF_COOKIE_NAME = "XSRF_TOKEN"
-
-# API Settings
-TASTYPIE_DEFAULT_FORMATS = ['json']
-TASTYPIE_ALLOW_MISSING_SLASH = True
-TASTYPIE_CANNED_ERROR = "Oops, the server just crapped. Our work monkeys have been notified."
-API_LIMIT_PER_PAGE = 50
-TASTYPIE_FULL_DEBUG = True
-
