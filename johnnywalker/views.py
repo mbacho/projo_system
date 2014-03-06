f914e91afd58e1ea4fc55619298af2af02fd1b4f
Chomba Ng'ang'a <choeringa@gmail.com>
2014-03-06 15:44:10 +0300

    Still removing scrapyd stuff

--- johnnywalker/views.py
+++ johnnywalker/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+
+@login_required
+def home(request):
+    return render_to_response('johnnywalker/home.html')
