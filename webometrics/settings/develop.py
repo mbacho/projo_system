60b2652c056951d6592f3d1b760d5ecf198c386c
Chomba Ng'ang'a <choeringa@gmail.com>
2014-03-31 01:40:06 +0300

    Add heroku config to production settings

--- webometrics/settings/develop.py
+++ webometrics/settings/develop.py
@@ -13,13 +13,12 @@ INSTALLED_APPS += (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'webometrics', # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'webometrics',
         'USER': 'webometrics',
         'PASSWORD': 'pass',
-        'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '', # Set to empty string for default.
+        'HOST': '',
+        'PORT': '',
     }
 }
 
--- webometrics/settings/production.py
+++ webometrics/settings/production.py
@@ -2,6 +2,8 @@
 Production django settings for webometrics project.
 """
 
+from os import getenv
+
 from .base import *
 
 DEBUG = False
@@ -9,12 +11,24 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'webometrics', # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'webometrics',
-        'PASSWORD': 'pass',
-        'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '', # Set to empty string for default.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd2itcuic6n4g8d',
+        'USER': 'tzgiqrvzdmayuo',
+        'PASSWORD': 'wxCQ6V_k8DVh0K2Qbd5UI0rIzn',
+        'HOST': 'ec2-23-21-101-129.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
+EMAIL_HOST = getenv('MAILTRAP_HOST')
+EMAIL_HOST_USER = getenv('MAILTRAP_USER_NAME')
+EMAIL_HOST_PASSWORD = getenv('MAILTRAP_PASSWORD')
+EMAIL_PORT = getenv('MAILTRAP_PORT')
+EMAIL_USE_TLS = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# MONGODB_URL = 'mongodb://webometrics:pass@oceanic.mongohq.com:10095/webometrics'
+
+# REDIS_URL = 'redis://redistogo:712486db5b7fc6f02b5282f6340e5883@grideye.redistogo.com:10222/'
+
